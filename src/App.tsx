2c5ea4d1357dd11df60fe55b5d7bdf201507d736
mrd010vergil <126393669+mrd010@users.noreply.github.com>
2024-05-03 21:06:12 +0330

    Create Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { CategoryPills } from './components/CategoryPills';
 import { categories, videos } from './data/home';
 import { PageHeader } from './layouts/PageHeader';
 import { VideoGridItem } from './components/VideoGridItem';
-import { Sidebar } from './components/Sidebar';
+import { Sidebar } from './layouts/Sidebar';
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
