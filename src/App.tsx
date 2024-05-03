7b2f72eee0f077d59016bac53ba63facd3eef102
mrd010vergil <126393669+mrd010@users.noreply.github.com>
2024-05-03 18:36:50 +0330

    Create mini sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { CategoryPills } from './components/CategoryPills';
 import { categories, videos } from './data/home';
 import { PageHeader } from './layouts/PageHeader';
 import { VideoGridItem } from './components/VideoGridItem';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
@@ -10,8 +11,8 @@ function App() {
     <div className="max-h-screen flex flex-col">
       <PageHeader></PageHeader>
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
-        <div>Sidebar</div>
-        <div className="overflow-x-hidden">
+        <Sidebar></Sidebar>
+        <div className="overflow-x-hidden px-10">
           <div
             className="sticky
            top-0 bg-white z-10 pb-4"
